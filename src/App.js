49658f1689480b503b61ad0efc7ac0b2989a888d
biprajit163 <biprajit163@gmail.com>
2021-02-27 19:57:23 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const base_url = 'http://127.0.0.1:8000'
+
+    let newUser = {
+      email: "a",
+      username: "a",
+      password: "a",
+      firstname: "a",
+      lastname: "a",
+      description: "a",
+      profile_picture: "https://picsum.photos/id/237/200/300"
+    }
+
+    axios.all([
+      axios.post(`${base_url}/users`, newUser),
+      axios.get(`${base_url}/users`)
+    ]).then(resArr => {
+      console.log(resArr[0])
+      console.log(resArr[1].data)
+    }).catch(err => console.log(err));
+
+    
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome Blogish!</h1>
     </div>
   );
 }
