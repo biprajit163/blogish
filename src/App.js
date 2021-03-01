9c991b96d54269ace223f46275eea75bb0632410
biprajit163 <biprajit163@gmail.com>
2021-03-01 02:42:19 -0500

    commit1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,79 @@
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [user_id, setUserId] = useState(null);
+  const [blog_id, setBlogId] = useState(null);
+  const [comment_id, setCommentId] = useState(null);
+
+  let userObj = {
+    email: "bob@email",
+    username: "",
+    password: "a",
+    firstname: "a",
+    lastname: "a",
+    description: "a",
+    profile_picture: "",
+  }
+
+  let blogObj = {
+    user: null,
+    title: "A New Blog",
+    image: "idk",
+    body: "This is a Blog",
+  }
+
+  let commentObj = {
+    blog: 2,
+    user: 7,
+    text: "First comment with frontend"
+  }
 
   useEffect(() => {
     const base_url = 'http://127.0.0.1:8000'
 
-    let newUser = {
-      email: "a",
-      username: "a",
-      password: "a",
-      firstname: "a",
-      lastname: "a",
-      description: "a",
-      profile_picture: "https://picsum.photos/id/237/200/300"
-    }
+    
 
-    axios.all([
-      axios.post(`${base_url}/users`, newUser),
-      axios.get(`${base_url}/users`)
-    ]).then(resArr => {
-      console.log(resArr[0])
-      console.log(resArr[1].data)
-    }).catch(err => console.log(err));
+
+    // axios
+    //   .post(`${base_url}/users`, userObj)
+    //   .then(res => {
+    //     console.log(res.data)
+    //   })
+
+    axios
+      .get(`${base_url}/users`)
+      .then(res => {
+        console.log(res.data)
+        setUsers(res.data)
+        setUserId(res.data[0].id);
+
+        setBlogs(res.data[0].blogs)
+      })
+
+
+    // axios
+    //   .delete(`http://127.0.0.1:8000/blogs/2`)
+    //   .then(res => console.log(res));
+    
+    // axios
+    //   .post(`${base_url}/blogs`, blogObj)
+    //   .then(res => console.log(res));
+
+    // axios
+    //   .post(`${base_url}/comments`, commentObj)
+    //   .then(res => console.log(res.data));
 
     
+
   }, []);
+
+  
+
 
   return (
     <div className="App">
