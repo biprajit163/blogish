00ac8beabe70eed955a3f7f1e53195d438f1f153
biprajit163 <biprajit163@gmail.com>
2021-03-01 16:21:57 -0500

    add React-Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import axios from "axios";
 import { UserContext } from "./UserContext.jsx";
 import { useState, useEffect } from "react";
+import { Route, Link } from 'react-router-dom'; 
 
 import Home from './Components/Home.js';
 import Login from './Components/Login.js';
@@ -12,16 +13,35 @@ function App() {
   const base_url = 'https://blogish-api.herokuapp.com'
 
   const [state, setState] = useState({
-    username: 'Biprajit',
-    login: 'bips1996',
+    username: 'blogish_user',
+    login: 'blogish_login',
   });
 
   return (
     <div className="App">
-      <h1>Welcome Blogish!</h1>
-      <UserContext.Provider value={{ state, setState }}>
-        <Home/>
-      </UserContext.Provider>
+      <header>
+        <Link to="/home"/>
+        <Link to="/login"/>
+        <Link to="/" />
+      </header>
+
+      <main>
+        <Route exact path="/" render={() => {
+          return(
+            <h1>Welcome Blogish!</h1>
+          );
+        }}/>
+
+        <UserContext.Provider value={{ state, setState }}>
+          <Route path="/home" render={() => (
+            <Home/>
+          )}/>
+          
+          <Route path="/login" render={() => (
+            <Login/>
+          )}/>
+        </UserContext.Provider>
+      </main>
     </div>
   );
 }
