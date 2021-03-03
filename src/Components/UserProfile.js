c4be4319678339ea38094a99a67b2b43ed1fbde9
biprajit163 <biprajit163@gmail.com>
2021-03-03 12:16:24 -0500

    design profile page

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -8,13 +8,15 @@ import BlogCard from './BlogCard.js';
 
 
 function UserProfile() {
-
+    // const user_id = getCookie("user_id")
+    
     const {context, setContext} = useContext(UserContext);
     const [user, setUser] = useState({});
 
-    useEffect(() => {   
+    useEffect(() => { 
         const user_id = getCookie("user_id")
-
+        console.log(user_id)
+        
         axios.get(`${context.base_url}/users/${user_id}`)
             .then(res => {
                 console.log(res.data);
@@ -23,33 +25,49 @@ function UserProfile() {
 
     }, []);
 
+    function logout() {
+        eraseCookie("user_id")
+        window.location.reload();
+    }
 
     return (
-        <div className="UserProfile d-flex justify-content-between">
-            <div className="profile-container">
-                <div className="profile-image">
-                    <img src="dfgdfg" alt="Picture goes here"/>
-                    <h3>Profile Image</h3>
-                </div>
-                <div className="user-email">Email goes here</div>
-                <div className="user-firstname">User Firstname</div>
-                <div className="user-lastname">User Lastname</div>
-                <div className="user-description">User Description</div>
+        <div className="UserProfile d-flex">
+            {getCookie("user_id") === null ? null : (
+                <>
+                    <div className="user-profile-container row justify-content-center">
+                        <div className="col-lg-4">
+                            <div className="profile-container">
+                                <div className="profile-image">
+                                    <img src={`${user.profile_picture}`} alt="Picture goes here" className="profile-image-cover"/>
+                                    <h3>{user.username}</h3>
+                                    <button className="btn btn-primary">Change Picture</button>
+                                </div>
+                                <div className="user-email">{user.email}</div>
+                                <div className="user-name">{user.firstname} {user.lastname}</div>
+                                <div className="user-description">{user.description}</div>
 
-                <button className="btn btn-dark">Update Profile</button>
-                <button className="btn btn-dark">Delete Profile</button>
-                
-            </div>
-            <div className="user-blogs-container">
-                <div className="user-blogs d-flex justify-content-center">
-                    <div className="create-blog">
-                        <button className="btn btn-primary">Create A Blog</button>
+                                <div className="profile-buttons">
+                                    <button className="btn btn-dark">Update Profile</button>
+                                    <button className="btn btn-dark">Delete Profile</button>
+                                    <button className="btn btn-danger" onClick={logout}>Logout</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-8">
+                            <div className="user-blogs-container">
+                                <div className="user-blogs">
+                                    <div className="create-blog">
+                                        <button className="btn btn-primary">Create A Blog</button>
+                                    </div>
+                                </div>
+                                <BlogCard/>
+                                <BlogCard/>
+                                <BlogCard/>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <BlogCard/>
-                <BlogCard/>
-                <BlogCard/>
-            </div>
+                </>
+            )}
         </div>
     );
 }
